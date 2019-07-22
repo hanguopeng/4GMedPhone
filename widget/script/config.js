fadb96e2f4f2b980d45dfb19bc91cc74d5d63df0
siyunyan <739798064@qq.com>
2019-07-22 10:05:09 +0800

    配置文件提交

--- widget/script/config.js
+++ widget/script/config.js
@@ -1,13 +1,13 @@
 var cmcServer="http://doclever.cn:8090/mock/5ad3f5ff995b693f603c9eeb";
 //var localServer = "http://140.143.143.150:8085/cmc-server";
-var localServer = "http://172.17.100.1:8083/cmc-server";
+var localServer = "http://172.17.100.2:8083/cmc-server";
 //var localServer = "http://111.26.165.97:8083/cmc-server";
 //var localServer = "http://10.0.11.95:8085/cmc-server";
-// var localServer = "http://192.168.1.112:8085/cmc-server";
+// var localServer = "http://192.168.1.107:8085/cmc-server";
 //var localServer = "http://192.168.1.138:8085/cmc-server";
 
 
-var ws = "ws://172.17.100.1:8888/";
+var ws = "ws://172.17.100.2:8888/";
 var config={
     duration:3000,
     versionUpdateUrl: localServer+"/sys/appVersion/verification", //版本更新
